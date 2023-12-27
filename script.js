afe67db489ef6dd8f699483d11b47cb7c54380e9
maicaocaaa <maria.caocaa@gmail.com>
2023-12-27 23:52:49 +0100

    api function consulting ok

--- script.js
+++ script.js
@@ -19,32 +19,70 @@ const colors = {
 
 
 // 1 definir funcion asincrona
-//elegir el endpoint
+// {id}/, es una convención utilizada en algunas API REST para indicar que cierta parte de la URL es un marcador de posición o variable. Esto no es específico de JavaScript, sino más bien una convención en el diseño de API REST.
+// sin $ no funciona el id
+// 2 definir endpoint
+//3 decir donde vamos a guadar los datos recogidos por fetch y ponerle await
+//4 pasar los datos recogidos fetch a .json ambien con await
+//5 ponemos return para manejar los datos de la pokeData
 
-function getPokemons() {
-	const endPoint = "https://pokeapi.co/api/v2/pokemon/"
-	let response = fetch(endPoint);
-	return response
-	};
+const getPokemon = async (id) => {
+	const endPoint = `https://pokeapi.co/api/v2/pokemon/${id}/`
+	const response = await fetch(endPoint)
+	const pokeData = await response.json()
+	
+	return pokeData
+};
+
+const getPokemons = async (num) => {
+	for (let i = 1; i<=num; i++) {
+		const pokeData = await getPokemon(i);  //lo pongouna constante porque sino devuelve promesas
+		
+		const poke_number = `# ${pokeData.id.toString().padStart(3, 0)}`   // lo pasa a string 001
+		const poke_name = pokeData.name
+		const poke_type = pokeData.types[0].type.name
+		const poke_image = pokeData.sprites.other['official-artwork']['front-default']  // es con [] no puede haber guiones 
+		console.log(poke_number, poke_name,poke_type);
+	}
+}
+
+getPokemons(pokemon_count);
+
+// const createPokeCard = () =>{
+//   const
+// }
 
 
 
+//const pokemon_count = 150
+//const poke-number
+//const poke-name
+//const poke-type
+//const poke-image
 
-
-
-console.log( getPokemons());
 
 // const createPokemonCard =
-// 		` <div class="pokemon" style="background-color: rgb(222,253,224);">
+// 		` <div class="pokemon" style="background-color: ${color.[poke-type]};">
 // 			<div class="img-container">
-// 				<img src="" alt="Bulbasaur">
+// 				<img src="${poke-image}" alt="${poke-name}>
 // 			</div>
 // 			<div class="info">
-// 				<span class="number">001</span>
-// 				<h3 class="name">Bulbasaur</h3>
-// 				<small class="type">Type: <span>grass</span></small>
+// 				<span class="number">${poke-number}</span>
+// 				<h3 class="name">${poke-name}</h3>
+// 				<small class="type">Type: <span>${poke-type}</span></small>
 // 			</div>
 // 		</div>`
 		
 // charactersContainer.innerHTML += createPokemonCard;
 
+
+
+
+// function getPokemons() {
+// 	const endPoint = "https://pokeapi.co/api/v2/pokemon/"
+// 	let response = fetch(endPoint);
+// 	return response
+// 	};
+
+
+// console.log( getPokemons());
